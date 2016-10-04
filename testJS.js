ddfc65bbef23512c9423036aaecffef477b772f2
Vladimir9999 <v.moroz@andersenlab.com>
2016-10-04 10:39:45 +0300

    add class User

--- testJS.js
+++ testJS.js
@@ -1,30 +1,57 @@
 /*
  1. Создать класс Publication с полями название, содержание, а также методы для изменения данных
  2. Создать класс User c полями ФИО, публикации, а также методы для изменения\добавления данных
- 3. Создать класс View для отображения данных о пользователях (имя, кол-во публикаций), отображение списка пользователей опционально должно принимать функцию сортировки
+ 3. Создать класс View для отображения данных о пользователях (имя, кол-во публикаций),
+ отображение списка пользователей опционально должно принимать функцию сортировки
  4. Создать страницу на которой будут отображены пользователи
  5. Все последовательные этапы создания приложения должны быть зафиксированы в недавно созданном репозитории на гитхаб
-*/
+ */
 //noinspection JSAnnotator
 
 'use strict';
-class Publication
-{
-    constructor(name, content)
-    {
-        this.name = name;
-        this.content = content;
-    }
-    set setName(newName)
-    {
-        this.name = newName;
-    }
-    set setContent(newContent)
-    {
-        this.content = newContent;
+class Publication {
+    constructor(name, content) {
+        this._name = name;
+        this._content = content;
     }
 
+    get name() {
+        return this._name;
+    }
 
+    set name(value) {
+        this._name = value;
+    }
+
+    get content() {
+        return this._content;
+    }
+
+    set content(value) {
+        this._content = value;
+    }
 }
 
+class User {
+    constructor(FIO, pulications) {
+        this._FIO = FIO;
+        this._pulications = pulications;
+    }
+
+    get FIO() {
+        return this._FIO;
+    }
+
+    set FIO(value) {
+        this._FIO = value;
+    }
+
+    get pulications() {
+        return this._pulications;
+    }
+
+    set pulications(value) {
+        this._pulications = value;
+    }
+}
 
