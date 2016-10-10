0f9a04a6a9e43f808c68502b18aac43059b37d2e
Vladimir9999 <v.moroz@andersenlab.com>
2016-10-10 10:39:34 +0300

    3

--- learnNodeJs/server.js
+++ learnNodeJs/server.js
@@ -4,90 +4,110 @@ var procces = require('process');
 var User = require('./User');
 var users = [];
 
-http.createServer(function (req, res){
-	if (req.url === '/') req.url = '/index.html';
-	
-	fs.readFile('./public'+req.url, 'utf8',  function(err, data){
-
-			res.writeHead(200, {'Content-Type' : 'text/html'});
-			if (err){
-				res.write('404: ');
-				res.end('Not found');
-			}
-			else{
-				console.log("Ok");
-				res.write(data);
-				res.end();
-			}
-			
-				
-	});	
-	
-}).listen(8888);
-
-procces.stdin.resume();
-process.stdin.on('data', function (chunk){
-	
-	var command = chunk.toString(),
-		command_array = command.split(' '),
-		str = command_array[command_array.length - 1];
-	command_array[command_array.length - 1] = str.substring(0, str.length - 2); // Удаление двух последних символов '\r\n'
-	
-	//console.log(command_array);
-	//Обработчик команд 
-	switch(command_array[0])
-	{
-		// load [file]
-		case 'load':{
-			fs.readFile('./public/' + command_array[1],'utf8',  function(err, data){
-				if (err)
-				{
-					console.log('ERROR! File not found');
-				}else
-				{
-					users = JSON.parse(data);
-					//console.log('Object- ' + users[0]._FIO + ' ' + users.length);
-					console.log(command_array[1] + ' loaded');
+function getFile(req, res){
+		if (req.url === '/') req.url = '/index.html';
+		fs.readFile('./public'+req.url, 'utf8',  function(err, data){
+				res.writeHead(200, {'Content-Type' : 'text/html'});
+				if (err){
+					res.write('404: Not found');
+					res.end();
 				}
+				else{
+					console.log("Ok");
+					res.write(data);
+					res.end();
+				}
+		});	
+}
+function appGet(req, res){
+		console.log('GET');
+		var url = req.url.split('/');
+		console.log(url);
+		if (url[1] === 'listUser')
+		{
+			if (!url[2])
+			{
+				if (users.length == 0) res.write('list empty');
+				for(var i = 0; i < users.length; i++){
+					res.write(i + ': ' + users[i].name + '\r\n');
+					console.log(users[i].name);
+				}
+			}else{
+				var index = parseInt(url[2], 10);
+				if (index >= users.length) {
+					res.write('User is not found');
+				}
+				else
+					res.write(index + ': ' + users[index].name + '\r\n');
+			}
+			res.end();
+		}
+		else{
+			getFile(req, res);
+		}	
+}
+// add/update
+function appPost(req, res){
+		console.log(req.method);
+		var postData = ""; 
+		req.addListener("data", function(postDataChunk) { 
+			postData += postDataChunk; 
+		}); 
 
-			});
-			break;
-		}		
-		// insert user [name]
-		case 'insert':{
-			if (command_array[1] == 'user'){
-				users[users.length] = new User(command_array[2]);
-				console.log('user: ' + users[users.length - 1]._FIO + ' added');
+		req.addListener("end", function() {
+			var url = req.url.split('/');
+			console.log(url);			
+			if (url[1] == 'addUser'){
+				users[users.length] = JSON.parse(postData);		
+				console.log(users[users.length - 1]);				
 			}
-			else console.log('\'' + command_array[1] + '\'' + ' is not defined');
-			break;
-		}
-		//update user index [name]
-		case 'update':{
-			if (command_array[1] == 'user' && command_array[2] < users.legth){
-				var tmp = users[command_array[2]]._FIO;
-				users[command_array[2]]._FIO = command_array[3];
-				console.log('old user ' + tmp + ' new user ' + users[command_array[2]]._FIO)
+			else if (url[1] = 'updateUser'){
+				if(url[2]){
+					var index = parseInt(url[2], 10);
+					if (index >= users.length) {
+						res.write('User is not found');
+					}					
+					else{
+						users[users.index] = JSON.parse(postData);
+						console.log(users[users.length - 1]);
+					} 						
+				}
+			}			
+			res.end();
+			
+	    });				
+}
+// Delete
+function appDel(req, res){
+	var url = req.url.split('/');
+	console.log(url);
+	if (url[1] = 'Del'){
+		if(url[2]){
+			var index = parseInt(url[2], 10);
+			if (index >= users.length) res.write('user is not found');
+			else {
+				res.write('Delete user - ' + users[index].name);
+				users[index] = {}; // Изменить на нормальное удаление				
 			}
-			else console.log('\'' + command_array[1] + '\'' + ' is not defined');
+		} 
+	} else res.end('Error'); 
+	res.end(); 
+}
+
+http.createServer(function (req, res){
+
+	switch(req.method){
+		case 'GET':
+			appGet(req, res);
 			break;
-		}
-		//delte
-		case 'delete':{
-			console.log(users.pop()._FIO + ' delete')
+		case 'POST':
+			appPost(req, res)
 			break;
-		}
-		//list
-		case 'list':{
-			console.log('users list');
-			if (users.length == 0) console.log('list empty');
-			for(var i = 0; i < users.length; i++){
-				console.log(i + ': ' + users[i]._FIO + '\r\n');				
-			}
+		case 'DELETE':
+			appDel(req, res);
 			break;
-		}
-		default: console.log('\'' + command_array[0] + '\'' + ' is not defined');
 	}
-});
 
+
+}).listen(8888);
 console.log('Server running on 8888');
