8f99524741b70578e4c0d2f26128340e7cf62be8
Vladimir9999 <v.moroz@andersenlab.com>
2016-10-18 10:50:16 +0300

    Routes, template

--- node/app.js/routes/index.js
+++ node/app.js/routes/index.js
@@ -4,10 +4,6 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next){
   res.render('index');
-  /*var path = './views/index';
-   res.sendFile(path, options, function(err) {
-   if (err) next(new HttpError());
-   });*/
 });
 
 module.exports = router;
