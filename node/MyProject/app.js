41126cb865c935db77aa4189c9784e0e71f10ee0
Vladimir9999 <v.moroz@andersenlab.com>
2016-10-19 13:01:26 +0300

    Bootstrap

--- node/MyProject/app.js
+++ node/MyProject/app.js
@@ -15,8 +15,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var app = express();
 
-//var multer = require('multer'); // v1.0.5
-//var upload = multer(); // for parsing multipart/form-data
+
 
 
 app.use(bodyParser.json());
