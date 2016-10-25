7d817addf66b6d55c69b047ed2c9526ad8440044
Vladimir9999 <v.moroz@andersenlab.com>
2016-10-25 16:57:21 +0300

    2

--- com.propertycross/js/Router.js
+++ com.propertycross/js/Router.js
@@ -1,42 +1,61 @@
 var lUrl = 'http://api.nestoria.com.br/api?encoding=json&pretty=1&action=search_listings&country=br&listing_type=buy&place_name=';
 'use strict';
+
+function getModels(location){
+    $.get(lUrl + location, function (data) {
+        if (data.response.application_response_code > 200) {
+            $("#title").text("Location not found!");
+            return;
+        }
+        let prop_col = new Properties(data["response"].listings);
+        for (let i = 1; i < prop_col.length; i++) {
+            let view = new PropertyView({
+                $el: "#searchProperty",
+                model: prop_col.get('c' + i)
+            });
+            $("#propertyList").append(view.el);
+        }
+    })
+    $("#title").text("Property in " + location);
+}
+
 var Router = Backbone.Router.extend({
     routes: {
-        "#": "home",
+        "": "home",
         "property/:query": "property",
         "search": "search",
+        "search/:query": "search",
         "faves": "faves"
     },
     initialize: function () {
         Backbone.history.start({pushState: false});
     },
     home: function(){
-        console.log("1");
         $('.hero-unit').hide();
-        $("#title").text("Use the page below to search for houses to buy.");
+        $("#title").text("Use the page to search for houses to buy.");
     },
     property: function () {
         $('.hero-unit').hide();
         $('#listProperty').show();
     },
-    search: function () {
+    search: function (query) {
         $('.hero-unit').hide();
         $('#searchProperty').show();
-        let location = $('#input').val();
-        if (!location) return;
-        $.get(lUrl + location, function (data) {
-            var prop_col = new Properties(data["response"].listings);
-            for (let i = 1; i < prop_col.length; i++) {
-                let view = new PropertyView({
-                    $el: "#searchProperty",
-                    model: prop_col.get('c' + i)
-                });
-                $("#propertyList").append(view.el);
+        if (query){
+            getModels(query);
+        }else {
+            let location = $('#input').val();
+            if (!location) {
+                $("#title").text("Location not found!");
+                return;
             }
-        })
+            getModels(location);
+        }
     },
     faves: function () {
+        $("#title").text("Favourite property");
         $('.hero-unit').hide();
-        $('#favesProperty').show();
+        //$('#favesProperty').show();
     }
 });
+
